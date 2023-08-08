d500e6af60ce55b66c9d0ffc90824bcafd0296d4
Ra2ieh <razieh.naeemi@outlook.com>
2023-08-08 11:52:35 +0430

    add console log

--- EfSample.Infrastructure/InfrastructureServiceInstaller.cs
+++ EfSample.Infrastructure/InfrastructureServiceInstaller.cs
@@ -4,11 +4,15 @@ public class InfrastructureServiceInstaller:IServiceInstaller
 {
  public void InstallServices(IServiceCollection services, IConfiguration appSettings, Assembly startupProjectAssembly)
     {
+        var optionBuilder=new DbContextOptionsBuilder();
+        optionBuilder.LogTo(Console.WriteLine);
         services.AddScoped<IUnitOfWork, UnitOfWork>();
         services.AddScoped<ICourseService, CourseService>();
         services.AddDbContext<CourseDbContext>(options =>
         options.UseSqlServer(appSettings.GetConnectionString("CourseDbConectionString"))
+        
         );
+       
         //var appConfig = appSettings.GetSection(nameof(AppConfig)).Get<AppConfig>();
         //services.AddHttpClientByConfig(appConfig.HttpClientConfigs.IranSign);
     }
