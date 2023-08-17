cd93c40af53c448a2b43be4eb3612512b4849bab
Ra2ieh <razieh.naeemi@outlook.com>
2023-08-17 18:40:54 +0430

    adding hosting extensions

---
+++ EfSample.Api/HostingExtensions.cs
@@ -0,0 +1,59 @@
+﻿namespace EfSample.Api
+{
+    public static class HostingExtensions
+    {
+        public static WebApplication ConfigureService(this WebApplicationBuilder builder)
+        {
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+            builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(GetCourseWithTeahcersDetailQueryHandler).Assembly));
+            builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(CourseWithTeachersAndTagsDetailQueryHandler).Assembly));
+
+            //builder.Services.AddMediatR(typeof(GetCourseWithTeahcersDetailQueryHandler).Assembly);
+            builder.Services.InstallServicesInAssemblies(builder.Configuration);
+            return builder.Build();
+        }
+        public static WebApplication ConfigurePileLine(this WebApplication app)
+        {
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            //
+            app.Map("/admin", myapp =>
+            {
+                myapp.UseMiddleware<ResponseDtoMiddleware>();
+
+
+            }
+            );
+            app.Use(async (httpContext, next) =>
+            {
+                if (httpContext.Request.Method == HttpMethod.Get.ToString())
+                {
+                    httpContext.Response.Headers.Add("Accept", "Razieh's Accept");
+                }
+                await next();
+            });
+            // app.UseMiddleware<ResponseDtoMiddleware>();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+            
+            return app;
+        }
+    }
+}
--- EfSample.Api/Program.cs
+++ EfSample.Api/Program.cs
@@ -1,62 +1,10 @@
-
-
 namespace EfSample.Api;
 public class Program
 {
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-        builder.Services.AddRazorPages();
-        //builder.Services.AddDbContext<CourseDbContext>(options =>
-        //{
-        //    options.UseSqlServer(builder.Configuration.GetConnectionString("CourseDbConectionString"));
-        //});
-        builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(GetCourseWithTeahcersDetailQueryHandler).Assembly));
-        builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(CourseWithTeachersAndTagsDetailQueryHandler).Assembly));
-
-        //builder.Services.AddMediatR(typeof(GetCourseWithTeahcersDetailQueryHandler).Assembly);
-        builder.Services.InstallServicesInAssemblies(builder.Configuration);
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment())
-        {
-            app.UseExceptionHandler("/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
-        }
-        //
-        app.Map("/admin", myapp =>
-         {
-             myapp.UseMiddleware<ResponseDtoMiddleware>();
-             
-
-         }
-        );
-        app.Use(async (httpContext, next) =>
-        {
-            if(httpContext.Request.Method == HttpMethod.Get.ToString())
-            {
-                httpContext.Response.Headers.Add("Accept", "Razieh's Accept");
-            }
-            await next();
-        });
-       // app.UseMiddleware<ResponseDtoMiddleware>();
-        app.UseHttpsRedirection();
-        app.UseStaticFiles();
-
-        app.UseRouting();
-
-        app.UseAuthorization();
-
-        app.MapRazorPages();
-        app.UseEndpoints(endpoints =>
-        {
-            endpoints.MapControllers();
-        });
+       var app= builder.ConfigureService().ConfigurePileLine();
         app.Run();
     }
 }
