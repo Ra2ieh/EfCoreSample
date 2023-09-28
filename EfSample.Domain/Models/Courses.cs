fb4828321d88608bb8ad992461ce84de5c784516
Ra2ieh <razieh.naeemi@outlook.com>
2023-09-28 19:04:15 +0330

    adding constructor to entity adding ILazyLoader

--- EfSample.Domain/Models/Courses.cs
+++ EfSample.Domain/Models/Courses.cs
@@ -1,11 +1,21 @@
-﻿namespace EfSample.Domain.Models;
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace EfSample.Domain.Models;
 public  class Course
 {
+    private readonly ILazyLoader lazyLoader;
+
+    public Course(ILazyLoader lazyLoader,string title)
+    {
+        Title ="title is :"+ title;
+        this.lazyLoader = lazyLoader;
+    }
     public int CourseId { get; set; }
     public string Title { get; set; } = string.Empty;
     public decimal Price { get; set; }
     public DateTime StartDate { get; set; }
-    public ICollection<CourseTeachers> CourseTeachers { get; set; }
+    private ICollection<CourseTeachers> _courseTeachers;
+    public ICollection<CourseTeachers> CourseTeachers { get =>lazyLoader.Load(this,ref _courseTeachers); set=>_courseTeachers=value; }
     public ICollection<CourseTag> Tags { get; set; }
     public ICollection<Comment> Comments { get; set; }
     public Discount Discount { get; set; }
--- EfSample.Domain/Models/Teachers.cs
+++ EfSample.Domain/Models/Teachers.cs
@@ -1,6 +1,10 @@
 ﻿namespace EfSample.Domain.Models;
 public class Teacher
 {
+    public Teacher()
+    {
+        LastName = "****";
+    }
     public int TeacherId { get; set; }
     public string FirstName { get; set; }=String.Empty;
     public string LastName { get; set; }= String.Empty; 
