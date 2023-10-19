f292c364fd5bcb8bc7d022145baed5db539160fa
Ra2ieh <razieh.naeemi@outlook.com>
2023-10-19 11:09:45 +0330

    add sql functions

---
+++ EfSample.Infrastructure/Migrations/20231019073240_sql-functions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EfSample.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class sqlfunctions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("CREATE FUNCTION GetCourseCommentsCount (@id int) RETURNS int AS BEGIN return (select count(*) from[dbt].[Comment] where CourseId = @id ) END");
+            migrationBuilder.Sql("CREATE FUNCTION GetCourseTagsCount (@id int) RETURNS int AS BEGIN return(select count(*) from [dbt].[CourseTag] where CourseId=@id) END");       
+            migrationBuilder.Sql("CREATE FUNCTION GetCourseComments (@id int) RETURNS table AS return( select * from [dbt].[Comment] where CourseId=@id)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("drop function GetCourseCommentsCount ");
+            migrationBuilder.Sql("drop function GetCourseTagsCount ");
+            migrationBuilder.Sql("drop function GetCourseComments ");
+        }
+    }
+}
